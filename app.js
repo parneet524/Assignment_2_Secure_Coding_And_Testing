37437f205d3f7bcd263f5d28c2284a8e458d21b1
Parneet <parneetazu@gmail.com>
2026-03-03 17:52:21 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello from JavaScript");
+function validateName(name) {
+  return name && name.trim() !== ''
+}
+
+function runApp() {
+  const name = 'Sample User'
+
+  if (validateName(name)) {
+    console.log('Hello, ' + name)
+  } else {
+    console.log('Invalid name')
+  }
+}
+
+runApp()
